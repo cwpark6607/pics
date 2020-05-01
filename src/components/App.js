54cf3b9cc26be5e24ec18191ba6cfbfceb93ec41
Chulwoo Park <cwpark6607@gmail.com>
2020-05-01 21:05:44 +0900

    implement Ajax request using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  onSearchSubmit = (term) => {
+    axios.get('https://api.unsplash.com/search/photos', {
+      headers: {
+        Authorization: 'Client-ID yd7FE0Da0UzYaiRS8CVJ4jC0UBQjWa1UAAh4oyqbuF0'
+      },
+      params: {
+        query: term
+      },
+    });
+  }
+
   render() {
     return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>);
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={ this.onSearchSubmit } />
+      </div>
+    );
   }
 }
 
