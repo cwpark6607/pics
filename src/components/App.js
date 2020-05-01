5a40dfdfd4c5134502fe0336ac2c6899ea524f98
Chulwoo Park <cwpark6607@gmail.com>
2020-05-02 00:29:02 +0900

    handling async & await

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit = (term) => {
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] }
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       headers: {
         Authorization: 'Client-ID yd7FE0Da0UzYaiRS8CVJ4jC0UBQjWa1UAAh4oyqbuF0'
       },
@@ -12,12 +13,15 @@ class App extends React.Component {
         query: term
       },
     });
+    
+    this.setState({ images: response.data.results});
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={ this.onSearchSubmit } />
+        Found {this.state.images.length} images
       </div>
     );
   }
